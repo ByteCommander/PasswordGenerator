1725cdd609258fe7917fcdb5399d71f48d9056f2
ByteCommander <bytecommander@gmx.de>
2016-07-03 21:58:27 +0200

    version 2: add command line parameters

--- pwgen.py
+++ pwgen.py
@@ -1,6 +1,103 @@
+#!/usr/bin/env python3
+
+import sys
 import string
 import random
 import math
+import argparse
+
+
+def main():
+    args = parse_args()
+
+    if args is None:
+        interactive()
+    else:
+        automatic(args)
+
+
+def automatic(args):
+    charset = make_charset(args.use_upper, args.use_lower, args.use_digits, args.use_punctuation, args.use_space,
+                           args.additional, args.blacklist)
+
+    if not args.quiet:
+        print("***** Password Generator - © 2016 ByteCommander *****")
+        print()
+        print("Using this character set (excluding the arrows):")
+        print("→{}←".format("".join(sorted(charset))))
+        print("There may be at most {} occurrences of the same character per password.".format(args.max_dupe)
+              if args.max_dupe > 0 else "There are no duplicate character limits.")
+        print()
+        print("Generating {} password{} of length {}:".format(args.amount, "s" if args.amount > 1 else "", args.length))
+        print()
+
+    for _ in range(args.amount):
+        password = generate_password(charset, args.length, args.max_dupe)
+        print(password)
+
+
+def interactive():
+    print("***** Password Generator - © 2016 ByteCommander *****")
+    print()
+
+    charset = ask_charset(True, True, True, True, True, "", "")
+    length = ask_length(12)
+    max_duplicate_chars = ask_max_duplicate_chars(0, len(charset), length)
+
+    while True:
+        password = generate_password(charset, length, max_duplicate_chars)
+
+        print("And your new password is:")
+        print("+-" + len(password) * "-" + "-+")
+        print("| " + password + " |")
+        print("+-" + len(password) * "-" + "-+")
+
+        if not ask_yn("Generate another password with the same settings?", True):
+            break
+
+    print("Thank you for using this password generator. Have a nice day!")
+
+
+def parse_args():
+    if len(sys.argv) <= 1:
+        # no arguments --> interactive mode
+        return None
+
+    parser = argparse.ArgumentParser(description="Highly customizable random password generator",
+                                     epilog="Run it without any arguments for interactive mode.")
+
+    parser.add_argument("length", action="store", type=int,
+                        help="length of the password to generate")
+    parser.add_argument("-n", "--amount", action="store", type=int, dest="amount", default=1,
+                        help="how many passwords to create")
+    parser.add_argument("-m", "--max-duplicate-chars", action="store", dest="max_dupe", default=0, metavar="LIMIT",
+                        help="limits how often the same character may occur in a password at most")
+
+    parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
+                        help="print only one password per line, nothing else")
+
+    p_charset = parser.add_argument_group("Character set specification")
+    p_charset.add_argument("-u", "--uppercase", action="store_true", dest="use_upper",
+                           help="include uppercase letters A-Z into the available character set")
+    p_charset.add_argument("-l", "--lowercase", action="store_true", dest="use_lower",
+                           help="include lowercase letters a-z into the available character set")
+    p_charset.add_argument("-d", "--digits", action="store_true", dest="use_digits",
+                           help="include digits 0-9 into the available character set")
+    p_charset.add_argument("-p", "--punctuation", action="store_true", dest="use_punctuation",
+                           help="include punctuation into the available character set".format(string.punctuation))
+    p_charset.add_argument("-s", "--space", action="store_true", dest="use_space",
+                           help="include the standard space into the available character set")
+
+    p_charset.add_argument("-a", "--additional", action="store", default="", dest="additional",
+                           help="additional characters to include into the available character set")
+    p_charset.add_argument("-b", "--blacklist", action="store", default="", dest="blacklist",
+                           help="characters to exclude from the available character set")
+
+    args = parser.parse_args()
+    if not any([args.use_upper, args.use_lower, args.use_digits, args.use_punctuation, args.use_space,
+               args.additional]):
+        parser.error("You must enable at least one character class or add custom characters!")
+    return args
 
 
 def ask_yn(message, default):
@@ -114,28 +211,6 @@ def generate_password(charset, length, max_duplicate_chars):
                 if password.count(c) >= max_duplicate_chars:
                     my_charset.discard(c)
     return password
-
-
-def main():
-    print("***** Password Generator - © 2016 ByteCommander *****")
-    print()
-
-    charset = ask_charset(True, True, True, True, True, "", "")
-    length = ask_length(12)
-    max_duplicate_chars = ask_max_duplicate_chars(0, len(charset), length)
-
-    while True:
-        password = generate_password(charset, length, max_duplicate_chars)
-
-        print("And your new password is:")
-        print("+-" + len(password) * "-" + "-+")
-        print("| " + password + " |")
-        print("+-" + len(password) * "-" + "-+")
-
-        if not ask_yn("Generate another password with the same settings?", True):
-            break
-
-    print("Thank you for using this password generator. Have a nice day!")
 
 
 if __name__ == "__main__":
